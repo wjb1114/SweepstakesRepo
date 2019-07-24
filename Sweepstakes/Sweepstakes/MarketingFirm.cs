13e96dd84f652f94494f66fc624bc3eaba35d231
wjb1114 <kbd1114@gmail.com>
2019-07-24 11:58:36 -0500

    Added SweepstakesManagerFactory class, implemented ability to create a new manager of either type based on user input. Referenced Factory in MarketingFirm class inside constructor.

--- Sweepstakes/Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/Sweepstakes/MarketingFirm.cs
@@ -8,5 +8,26 @@ namespace Sweepstakes
 {
     class MarketingFirm
     {
+        ISweepstakesManager manager;
+        public MarketingFirm()
+        {
+            bool validInput;
+            do
+            {
+                Console.WriteLine("Will you use a stack or a queue?");
+                string input = Console.ReadLine();
+                try
+                {
+                    manager = SweepstakesManagerFactory.GetManager(input);
+                    validInput = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    validInput = false;
+                }
+            }
+            while (validInput == false);
+        }
     }
 }
---
+++ Sweepstakes/Sweepstakes/SweepstakesManagerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    static class SweepstakesManagerFactory
+    {
+        public static ISweepstakesManager GetManager(string input)
+        {
+            switch(input.ToLower())
+            {
+                case "stack":
+                    return new SweepstakesStackManager();
+                case "queue":
+                    return new SweepstakesQueueManager();
+                default:
+                    throw new ApplicationException("Invalid user input.");
+            }
+        }
+    }
+}
