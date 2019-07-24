9b1d1f6afe08016918aeb6988d618ddd5ca8b8e3
wjb1114 <kbd1114@gmail.com>
2019-07-24 11:01:01 -0500

    Added Contestant class.

---
+++ Sweepstakes/Sweepstakes/Contestant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        string firstName;
+        string lastName;
+        string emailAddress;
+        int registrationNumber;
+
+        public Contestant(string first, string last, string email, int idNumber)
+        {
+            firstName = first;
+            lastName = last;
+            emailAddress = email;
+            registrationNumber = idNumber;
+        }
+    }
+}
